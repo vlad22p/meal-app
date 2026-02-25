f7a53aff1d041f19c016c565ee9729be77d7efcf
Vlad <vlad.pop0797@gmail.com>
2026-02-25 14:54:19 +0200

    profile picture in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import "../styles/NavBar.css";
 
 export default function NavBar() {
+    const { user } = useUser();
+
     return (
         <nav className="nav-main-container">
             <div className="nav-logo-container">
@@ -12,6 +16,15 @@ export default function NavBar() {
             </div>
             <div className="nav-links-container">
                 <SignedIn>
+                    {user?.imageUrl ?
+                        (
+                            <Link href="/profile">
+                                <img src={user.imageUrl} className="nav-profile-picture" alt="Profile image" />
+                            </Link>
+                        ) : (
+                            <div>Profile</div>
+                        )
+                    }
                     <Link href="/mealplan">
                         Meal Plan
                     </Link>
